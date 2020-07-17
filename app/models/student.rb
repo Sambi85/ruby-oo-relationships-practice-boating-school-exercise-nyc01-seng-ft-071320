4a4851b6977c7626c4198c2a5802f8194ae3f099
github username <github email address>
2020-07-17 10:30:28 -0400

    edit boating test & student classes

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,43 @@
+require 'pry'
+
 class Student
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        Student.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(name, status, instructor)
+        BoatingTest.new(self, name, status, instructor)
+    end
+
+    def self.find_student
+    end
+
+    def student_tests
+        BoatingTest.all.select do |bt| 
+         student == self 
+        end
+    end
+
+    def passed_tests
+        student_tests.select do |bt|
+        bt.status == "passed"
+      end
+    end
+
+    def grade_percentage       
+        anwser = passed_tests.count/student_tests.count
+        anwser.to_f * 100
+    end 
+
 
 end
---
+++ console.rb
@@ -0,0 +1,6 @@
+require_relative 'app/models/boatingtest.rb'
+require_relative 'app/models/instructor.rb'
+require_relative 'app/models/student.rb'
+require 'pry'
+
+
